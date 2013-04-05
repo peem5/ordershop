9510cc7e939dc950d6c93a1cd418f8d6808435ba
Manfred Fischer <m.fischer@ssi-schaefer-peem.com>
2013-04-05 11:51:32 +0200

    delete patternjavafiles

--- ISpecification.java
+++
@@ -1,9 +0,0 @@
-package at.edu.hti.shop.specificationpattern;
-
-public interface ISpecification<T> {
-
-	boolean IsSatisfiedBy(T t);
-	ISpecification<T> and(ISpecification<T> specification);
-	ISpecification<T> or(ISpecification<T> specification);
-	ISpecification<T> not(ISpecification<T> specification);
-}
